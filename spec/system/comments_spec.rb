65aee7d1b7729e9b297557215505b7c3e1ece946
github peterdo415 <btsmanne@gmail.com>
2022-11-09 17:23:09 +0900

    要らないテストファイル削除、turboの記入漏れ追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -18,4 +18,31 @@ RSpec.describe "Comments", type: :system do
     end
   end
 
+  describe '更新' do
+    let!(:comment) { create(:comment, post: post, user: user) }
+    it 'コメントが更新できること' do
+      visit post_path(post)
+      within "#comment_#{comment.id}" do
+        find('.btn-edit').click
+      end
+      within "#form_comment_#{comment.id}" do
+        fill_in 'コメント', with: '更新コメント'
+        click_on '更新する'
+      end
+
+      expect(page).to have_content '更新コメント'
+    end
+  end
+
+  describe '削除' do
+    let!(:comment) { create(:comment, post: post, user: user) }
+    it 'コメントが削除できること' do
+      visit post_path(post)
+      within "#comment_#{comment.id}" do
+        accept_confirm {find('.btn-delete').click}
+      end
+      expect(page).to_not have_content comment.body
+    end
+  end
+
 end
